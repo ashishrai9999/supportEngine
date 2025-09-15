343ff13fcf6e5be421d5c491ac7d56a99c78e53a
ashish <ashish.rai@antrepriz.com>
2025-09-15 12:54:10 +0530

    url changed

--- public/app.js
+++ public/app.js
@@ -10,7 +10,7 @@ let isRecording = false;
 let audioContext = null;
 
 // Backend API configuration
-const BACKEND_URL = 'http://localhost:8080';
+const BACKEND_URL = 'https://idfc-mmt-stage.dice.tech/support';
 
 // DOM elements
 const elements = {
